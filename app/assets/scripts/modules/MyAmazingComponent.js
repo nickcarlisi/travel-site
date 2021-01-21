b58b6892488409756bba9f3aa0e0b7345381b880
Nick <nickcarlisi80@gmail.com>
2021-01-21 13:50:35 -0500

    updated for github pages

---
+++ app/assets/scripts/modules/MyAmazingComponent.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function MyAmazingComponent() {
+    return (
+        <div>
+            <h1>This is my amazing React component</h1>
+            <p>React is great!!!</p>
+        </div>
+    )
+}
+
+export default MyAmazingComponent
